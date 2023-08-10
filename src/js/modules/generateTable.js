37d2066eca2c0417259cd6fb1b355e442c2e48de
AleksandrMaslov <amaslov8910@gmail.com>
2023-08-10 22:29:56 +0300

    add task icons

--- src/js/modules/generateTable.js
+++ src/js/modules/generateTable.js
@@ -16,6 +16,9 @@ class TaskListAPI {
     this.headerClass = `${blockClass}__header`
     this.rowClass = `${blockClass}__row`
     this.cellClass = `${blockClass}__cell`
+    this.logoClass = `${blockClass}__logo`
+    this.actionsClass = `${blockClass}__actions`
+    this.actionClass = `${blockClass}__action`
     this.taskId = 'taskId'
     this.block = document.querySelector(`.${this.blockClass}`)
   }
@@ -30,30 +33,39 @@ class TaskListAPI {
     this.header = document.createElement('div')
     this.header.classList.add(`${this.hearedRowClass}`)
     const properties = Object.keys(Object.values(data)[0])
+
+    const headerLogo = document.createElement('div')
+    headerLogo.classList.add(`${this.logoClass}`)
+    this.header.appendChild(headerLogo)
+
     properties.forEach((header) => {
       const headerCell = document.createElement('div')
       headerCell.classList.add(`${this.headerClass}`)
       headerCell.innerHTML = `${header.toUpperCase()}`
       this.header.appendChild(headerCell)
     })
+
+    const headerActions = document.createElement('div')
+    headerActions.classList.add(`${this.actionsClass}`)
+    this.header.appendChild(headerActions)
+
     this.list.appendChild(this.header)
   }
 
   addRow(keyValue) {
     const [key, properties] = keyValue
-    const row = document.createElement('div')
-    row.classList.add(`${this.rowClass}`)
-    row.setAttribute(this.taskId, key)
-    row.onclick = row.remove
+    const row = this.createRow(key)
+    const rowLogo = this.createRowLogo()
+    row.appendChild(rowLogo)
 
     Object.values(properties).forEach((cellData) => {
       const data = `${cellData}`
-      const cell = document.createElement('div')
-      cell.classList.add(`${this.cellClass}`)
-      cell.setAttribute('title', data)
-      cell.innerHTML = data
+      const cell = this.createCell(data)
       row.appendChild(cell)
     })
+
+    const rowActions = this.createRowActions()
+    row.appendChild(rowActions)
     this.list.appendChild(row)
   }
 
@@ -65,5 +77,48 @@ class TaskListAPI {
 
   insertRow() {
     // parentElement.insertBefore(newElement, referenceElement);
+  }
+
+  createRow(key) {
+    const row = document.createElement('div')
+    row.classList.add(`${this.rowClass}`)
+    row.setAttribute(this.taskId, key)
+    row.onclick = row.remove
+    return row
+  }
+
+  createRowLogo() {
+    const rowLogo = document.createElement('div')
+    rowLogo.classList.add(`${this.logoClass}`)
+    const logo = document.createElement('img')
+    logo.setAttribute('src', 'img/tasks/tasks-marker.png')
+    logo.setAttribute('alt', 'tasks-marker')
+    rowLogo.appendChild(logo)
+    return rowLogo
+  }
+
+  createCell(data) {
+    const cell = document.createElement('div')
+    cell.classList.add(`${this.cellClass}`)
+    cell.setAttribute('title', data)
+    cell.innerHTML = data
+    return cell
+  }
+
+  createRowActions() {
+    const rowActions = document.createElement('div')
+    rowActions.classList.add(`${this.actionsClass}`)
+    rowActions.appendChild(this.createAction('edit'))
+    rowActions.appendChild(this.createAction('comment'))
+    rowActions.appendChild(this.createAction('start'))
+    return rowActions
+  }
+
+  createAction(name) {
+    const action = document.createElement('img')
+    action.classList.add(`${this.actionClass}`)
+    action.setAttribute('src', `img/tasks/tasks-${name}.png`)
+    action.setAttribute('alt', `tasks-${name}`)
+    return action
   }
 }
