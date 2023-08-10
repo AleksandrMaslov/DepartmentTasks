9254a2980d01b1908348474c25592a163420a7b3
AleksandrMaslov <amaslov8910@gmail.com>
2023-08-10 20:58:04 +0300

    refactor table generation without text

--- src/js/modules/generateTable.js
+++ src/js/modules/generateTable.js
@@ -16,46 +16,39 @@ class TaskListAPI {
     this.headerClass = `${blockClass}__header`
     this.rowClass = `${blockClass}__row`
     this.cellClass = `${blockClass}__cell`
-    this.cellWrapperClass = `${blockClass}__cell-wrapper`
     this.block = document.querySelector(`.${this.blockClass}`)
   }
 
   addTable() {
-    this.block.innerHTML = `<div class='${this.listClass}'></div>`
-    this.list = document.querySelector(`.${this.listClass}`)
+    this.list = document.createElement('div')
+    this.list.classList.add(`${this.listClass}`)
+    this.block.appendChild(this.list)
   }
 
   addHeader(data) {
-    this.list.innerHTML = `
-    <div class='${this.hearedRowClass}'>
-      <div class="${this.headerClass}">
-        ID
-      </div>
-    </div>`
-
-    this.header = document.querySelector(`.${this.hearedRowClass}`)
+    this.header = document.createElement('div')
+    this.header.classList.add(`${this.hearedRowClass}`)
     const properties = Object.values(data)[0]
-    Object.keys(properties).forEach((property) => {
-      this.header.innerHTML += `
-      <div class="${this.headerClass}">
-        ${property.toUpperCase()}
-      </div>`
+    const headers = ['id', ...Object.keys(properties)]
+    headers.forEach((header) => {
+      const headerCell = document.createElement('div')
+      headerCell.classList.add(`${this.headerClass}`)
+      headerCell.innerHTML = `${header.toUpperCase()}`
+      this.header.appendChild(headerCell)
     })
+    this.list.appendChild(this.header)
   }
 
   addRow(keyValue) {
     const [key, properties] = keyValue
     const row = document.createElement('div')
     row.classList.add(`${this.rowClass}`)
-    row.innerHTML = `
-    <div class="${this.cellClass}">
-      ${key}
-    </div>`
-    Object.values(properties).forEach((property) => {
-      row.innerHTML += `
-      <div class="${this.cellClass}">
-        ${property}
-      </div>`
+    const cellsData = [key, ...Object.values(properties)]
+    cellsData.forEach((cellData) => {
+      const cell = document.createElement('div')
+      cell.classList.add(`${this.cellClass}`)
+      cell.innerHTML = `${cellData}`
+      row.appendChild(cell)
     })
     this.list.appendChild(row)
   }
