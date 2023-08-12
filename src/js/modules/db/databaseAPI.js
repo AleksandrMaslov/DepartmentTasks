1ac16634f19508f156cf0d9a1c3f9028c257a3fa
AleksandrMaslov <amaslov8910@gmail.com>
2023-08-12 12:55:01 +0300

    set cell data request

--- src/js/modules/db/databaseAPI.js
+++ src/js/modules/db/databaseAPI.js
@@ -1,22 +1,45 @@
 export default class DatabaseAPI {
   SCRIPT_ID =
-    'AKfycbyy0KOn4EZZwAjtVeztBwgXlz_7dtIEoW_clMNe8HHmS_0NM94ID123osKpbuPruNMHfA'
+    'AKfycbwW1Dx_lkpFncP-7h5O6cG_jpYUJAszOl8TZGyCCZZqh7y1DPm_9jR5ndmyiaKOX3iAxA'
 
   constructor() {
     this.baseUrl = `https://script.google.com/macros/s/${this.SCRIPT_ID}/exec`
   }
 
   async requestTasks() {
-    const url = this.generateUrl({ sheet: 'TASKS' })
+    const url = this.getUrl({ sheet: 'TASKS' })
     const response = await fetch(url)
     return await response.json()
   }
 
-  generateUrl(params) {
+  async setCellData(key, header, value) {
+    const property = Object.fromEntries([[header, value]])
+    const row = Object.fromEntries([[key, property]])
+    const body = {
+      action: 'update',
+      data: row,
+    }
+
+    const url = this.getUrl({ sheet: 'TASKS' })
+    const options = this.getPostOptions(body)
+    const response = await fetch(url, options)
+    return await response.json()
+  }
+
+  getUrl(params) {
     const requestParams = Object.entries(params)
       .filter((parameter) => !parameter.includes(undefined))
       .map((parameter) => parameter.join('='))
       .join('&')
     return `${this.baseUrl}?${requestParams}`
+  }
+
+  getPostOptions(body) {
+    return {
+      redirect: 'follow',
+      method: 'POST',
+      headers: { 'Content-Type': 'text/plain;charset=utf-8' },
+      body: JSON.stringify(body),
+    }
   }
 }
