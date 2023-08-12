10b3c16e1a12a5fb26445316c0a5110b06e66da6
AleksandrMaslov <amaslov8910@gmail.com>
2023-08-12 12:53:51 +0300

    reorganize

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import defineWebp from './modules/common/defineWebp.js'
+import defineWebp from './modules/webp/defineWebp.js'
 import createTaskList from './modules/tasks/createTaskList.js'
 import defineModal from './modules/modal/defineModal.js'
 
