16e68ee0d5569d313db741097cd2c0203a18f6b4
AleksandrMaslov <amaslov8910@gmail.com>
2023-08-15 21:48:17 +0300

    comment form reset

--- src/js/modules/tasks/actions/commentAction.js
+++ src/js/modules/tasks/actions/commentAction.js
@@ -1,18 +1,7 @@
 export default function onComment() {
+  const commentForm = document.querySelector('.comment-form')
+  commentForm.reset()
+
   const modal = document.querySelector('.modal_comment')
   modal.style.display = 'flex'
 }
-// document.addEventListener('DOMContentLoaded', function () {
-//   var elements = document.getElementsByTagName('INPUT')
-//   for (var i = 0; i < elements.length; i++) {
-//     elements[i].oninvalid = function (e) {
-//       e.target.setCustomValidity('')
-//       if (!e.target.validity.valid) {
-//         e.target.setCustomValidity('This field cannot be left blank')
-//       }
-//     }
-//     elements[i].oninput = function (e) {
-//       e.target.setCustomValidity('')
-//     }
-//   }
-// })
