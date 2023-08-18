02778c7c9f75fc2a09941f93acbddb22bbb0c731
AleksandrMaslov <amaslov8910@gmail.com>
2023-08-18 19:28:12 +0300

    refactor with getNewState function

--- src/js/modules/actions/onStart.js
+++ src/js/modules/actions/onStart.js
@@ -5,18 +5,20 @@ export default async function onStart(event) {
   const header = 'isActive'
   const rowController = new TaskRowController(event)
   const key = rowController.getKey()
-
   const currentState = rowController.getCellData(header)
-  rowController.updateCellData(header, '.')
 
+  rowController.updateCellData(header, '.')
+  const state = await getNewState(key, header, currentState)
+  rowController.updateCellData(header, state)
+}
+
+async function getNewState(key, header, currentState) {
   const data = await new DatabaseController().switchState(
     key,
     header,
     currentState
   )
-
   const { result, report } = data
   const { value: newState } = report
-  const state = result === 'success' ? newState : currentState
-  rowController.updateCellData(header, state)
+  return result === 'success' ? newState : currentState
 }
