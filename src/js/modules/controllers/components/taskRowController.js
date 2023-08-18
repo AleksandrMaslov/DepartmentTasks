9fbe9da436a671e9f069e99820c782299bca4127
AleksandrMaslov <amaslov8910@gmail.com>
2023-08-18 22:15:29 +0300

    remove unsued properties

--- src/js/modules/controllers/components/taskRowController.js
+++ src/js/modules/controllers/components/taskRowController.js
@@ -6,13 +6,8 @@ export default class TaskRowController {
     this.rowClass = 'row'
     this.cellClass = `${this.rowClass}__cell`
     this.cellContentClass = `${this.rowClass}__cell-content`
-    this.actionClass = `${this.rowClass}__action`
-    this.actionContentClass = `${this.rowClass}__action-content`
-    this.loaderClass = `${this.rowClass}__loader`
 
     this.actionContent = event.srcElement
-    this.action = this.actionContent.closest(`.${this.actionClass}`)
-
     this.row = this.actionContent.closest(`.${this.rowClass}`)
     this.key = this.row.getAttribute('key')
   }
